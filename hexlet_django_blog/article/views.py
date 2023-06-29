d9569a670fb753cf3a22f3fb88b52c4d4587c164
savedim <savedimdev@gmail.com>
2023-06-29 13:59:52 +0300

    Add template to articles

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
 def index(request):
-    return HttpResponse('article')
+    title = 'Заголовок'
+    description = 'Описание'
+    created_at = datetime.date.today
+    return render(request, 'articles/index.html', context={
+        'title': title, 'description': description,
+        'created_at': created_at,
+    })
