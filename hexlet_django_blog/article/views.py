7b4bc12cdab19c544b40fd50548d203b71c2dd33
savedim <savedimdev@gmail.com>
2023-06-30 15:22:12 +0300

    Add classes for index

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
+from django.views import View
 
-def index(request):
-    title = 'Заголовок'
-    description = 'Описание'
-    created_at = datetime.date.today
-    return render(request, 'articles/index.html', context={
-        'title': title, 'description': description,
-        'created_at': created_at,
-    })
+class IndexView(View):
+
+    def get(self, request, *args, **kwargs):
+        title = 'Заголовок'
+        description = 'Описание'
+        created_at = datetime.date.today
+        return render(request, 'articles/index.html', context={
+            'title': title, 'description': description,
+            'created_at': created_at,
+        })
